bae76563d66381af683a57f1b531724f94bfb0eb
vmlganesh <vmlganesh@users.noreply.github.com>
2022-12-01 12:46:45 +0530

    first commit

---
+++ src/components/Users.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Users(props) {
+  
+  return (
+    <div className="col-lg-2">
+      <div className="card mt-3">
+        <img className="card-img-top" src={props.employee_image} />
+        <div className="card-body">
+          <h5 className="card-title">{props.e_first_name} {props.e_last_name}</h5>
+          <p className="card-text">{props.e_email_address}</p>
+          {/* <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a> */}
+          
+          <input type="button" className="btn btn-primary" value={props.btn_value} onClick={() => { props.onPress(props.employee_id) }}/>
+        </div>
+      </div>
+    </div>
+  )
+}
